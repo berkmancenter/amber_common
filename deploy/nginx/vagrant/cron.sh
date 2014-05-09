f27ea3be8849fa2629250d32e5159789f6b41623
Jeffrey Licht <jeff@podconsulting.com>
2014-05-09 15:29:19 -0400

    Make cron job dequeue all items, not just one

--- deploy/nginx/vagrant/cron.sh
+++ deploy/nginx/vagrant/cron.sh
@@ -3,7 +3,7 @@
  PIDS=`ps aux | grep CAYLRunner.php | grep -v grep`
  if [ -z "$PIDS" ]; then
      echo "Starting CAYLRunner.php ..."
-     php /usr/local/src/robustness_common/src/CAYLRunner.php
+     while php /usr/local/src/robustness_common/src/CAYLRunner.php dequeue; do true ; done
  else
      echo "CAYLRunner.php already running."
  fi
