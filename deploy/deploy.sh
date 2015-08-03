83dbb7679f292a6864f63beb2c9159b3382d33cc
Jeffrey Licht <jlicht@podconsulting.com>
2015-08-03 15:33:34 -0400

    Allow deploying without assigning elastic IP addresses

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,13 +9,16 @@ function report () {
 }
 
 # Validation messages
-USAGE="Usage: deploy.sh --platform=[drupal|wordpress|nginx|apache|all] --release=RELEASE [--site-password=PASSWORD]"
+USAGE="Usage: deploy.sh --platform=[drupal|wordpress|nginx|apache|all] --release=RELEASE [--site-password=PASSWORD] [--set-virtual-ip=0|1]"
 ENVIRONMENT="The following environment variables must be set in order to spin up the AWS instance: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_KEYPAIR_NAME"
 
 # Get absolute path to the script
 pushd `dirname $0` > /dev/null
 SCRIPT_DIR=`pwd`
 popd > /dev/null
+
+# Set default values
+SET_VIRTUAL_IP=1
 
 # Save current path
 pushd . > /dev/null
@@ -33,6 +36,10 @@ case $i in
     ;;
     --site-password=*)
     SITE_PASSWORD="${i#*=}"
+    shift # past argument=value
+    ;;
+    --set-virtual-ip=*)
+    SET_VIRTUAL_IP="${i#*=}"
     shift # past argument=value
     ;;
     *)
@@ -87,9 +94,14 @@ for P in $PLATFORM; do
 	SERVER_URL="http://${!IP}" SITE_PASSWORD=$SITE_PASSWORD RELEASE_TAG=$RELEASE vagrant up --provider=aws
 	INSTANCE_ID=$(<`find .vagrant -name id`)
 
-	# Set the IP address for the new server to the elastic IP defined in $AMBER_[platform]_ELASTIC_IP
-	type ec2-associate-address >/dev/null 2>/dev/null && test "${!IP}" && ec2-associate-address ${!IP} --instance $INSTANCE_ID
-	report "Server available with $P release $RELEASE and instance id: $INSTANCE_ID and IP: ${!IP}"
+	if [ "${SET_VIRTUAL_IP}" -eq 1 ] 
+	then
+		# Set the IP address for the new server to the elastic IP defined in $AMBER_[platform]_ELASTIC_IP
+		type ec2-associate-address >/dev/null 2>/dev/null && test "${!IP}" && echo ec2-associate-address ${!IP} --instance $INSTANCE_ID && report "Server available with $P release $RELEASE and instance id: $INSTANCE_ID and IP: ${!IP}"
+	else
+		report "Server available with $P release $RELEASE and instance id: $INSTANCE_ID"
+	fi
+
 done
 	
 popd . > /dev/null
